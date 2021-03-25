043afb934380b5c573b7087f4c1f1ad3a6a71ee2
sowmya-yalamarthi <s542049@nwmissouri.edu>
2021-03-24 19:44:22 -0500

    changed

---
+++ routes/computation.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+/* GET home page. */
+router.get('/', function(req, res, next) {
+let someNumber=Math.floor(Math.random()*100)
+res.render('compute', { result:'log applied to '+someNumber+' is '+Math.pow(someNumber)});
+});
+module.exports = router;
